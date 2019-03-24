4da92c420f71d59ed6c4b80fdce9d80f7e32705a
Igor Anokhin <igor.anohin@emlid.com>
2019-03-24 05:50:56 +0300

    add dynamically genereted peoples PopUps

--- client/src/main/java/ru/ifmo/se/client/MainActivity.kt
+++ client/src/main/java/ru/ifmo/se/client/MainActivity.kt
@@ -1,21 +1,28 @@
 package ru.ifmo.se.client
 
 import android.Manifest
+import android.app.Dialog
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.graphics.Color.argb
+import android.graphics.PointF
 import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
+import android.support.v4.content.res.ResourcesCompat
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.util.Log
+import android.view.Gravity
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 import com.here.android.mpa.common.*
+import com.here.android.mpa.mapping.*
 import com.here.android.mpa.mapping.Map
-import com.here.android.mpa.mapping.MapMarker
-import com.here.android.mpa.mapping.SupportMapFragment
 import io.grpc.ManagedChannel
 import io.grpc.ManagedChannelBuilder
 import ru.ifmo.se.protofiles.CommunicatorGrpc
@@ -24,6 +31,7 @@ import ru.ifmo.se.protofiles.Musician
 import java.io.File
 import java.io.PrintWriter
 import java.io.StringWriter
+import java.lang.Math.abs
 import java.util.concurrent.TimeUnit
 
 
@@ -72,6 +80,211 @@ class MainActivity : AppCompatActivity() {
         if(success){
             mapFragment.init {
                 if (it == OnEngineInitListener.Error.NONE) {
+//                    mapFragment.mapGesture.addOnGestureListener(object : MapGesture.OnGestureListener.OnGestureListenerAdapter() {
+//                        override fun onMapObjectsSelected(objectss : List<ViewObject>) : Boolean {
+//                            for (viewObj in objectss)
+//                                if (viewObj.getBaseType() == ViewObject.Type.USER_OBJECT) {
+//                                    if ((viewObj as MapObject).getType() == MapObject.Type.MARKER) {
+//                                        // At this point we have the originally added
+//                                        // map marker, so we can do something with it
+//                                        // (like change the visibility, or more
+//                                        // marker-specific actions)
+//                                        (viewObj as MapObject).setVisible(false)
+//                                    }
+//                                }
+//                            // return false to allow the map to handle this callback also
+//                            return false
+//                            }
+//                        })
+                    mapFragment.mapGesture.addOnGestureListener(object : MapGesture.OnGestureListener {
+
+                        override fun onPanStart() { }
+
+                        override fun onPanEnd() {
+                            /* show toast message for onPanEnd gesture callback */
+                            Log.i("Fuck", "onPanEnd")
+                        }
+
+                        override fun onMultiFingerManipulationStart() { }
+                        override fun onMultiFingerManipulationEnd() { }
+                        override fun onMapObjectsSelected(list : List<ViewObject>) : Boolean {
+                            Log.i( "Fuck" ,"onTapEvent")
+                            Log.i( "ForFuck" ,"HERE!!!!!")
+                            for (viewObj in list)
+                                if (viewObj.getBaseType() == ViewObject.Type.USER_OBJECT) {
+                                    if ((viewObj as MapObject).getType() == MapObject.Type.MARKER) {
+                                        // At this point we have the originally added
+                                        // map marker, so we can do something with it
+                                        // (like change the visibility, or more
+                                        // marker-specific actions)
+                                        var tempMusicians = arrayListOf<Musician>().toTypedArray()
+                                        while (tempMusicians.size == 0)
+                                            if (!musicians.isEmpty()) {
+                                                tempMusicians = musicians.toTypedArray()
+                                            }
+                                        for (musician in tempMusicians) {
+                                            createPopUp(musician)
+                                            return false
+                                        }
+                                    }
+                                }
+                            /*
+                             * add map screen marker at coordinates of gesture. if map
+                             * screen marker already exists, change to new coordinate
+                             */
+//                    if (m_tap_marker == null) {
+//                        m_tap_marker = new MapScreenMarker(pointF,
+//                                m_marker_image);
+//                        m_map.addMapObject(m_tap_marker);
+//
+//                    } else {
+//                        m_tap_marker.setScreenCoordinate(pointF);
+//                    }
+
+                            // return false to allow the map to handle this callback also
+                            return false
+                        }
+
+                        private fun createPopUp(musician: Musician) {
+                            val pw = Dialog(this@MainActivity)
+                            pw.setContentView(R.layout.autor)
+//                            pw.setCanceledOnTouchOutside(false)
+                            pw.show()
+
+
+                            val singerName = pw.findViewById<TextView>(R.id.singer_name)
+                            singerName.text = musician.name
+
+                            val singer_styles = pw.findViewById<TextView>(R.id.singer_styles)
+                            singer_styles.text = musician.generesList.joinToString()
+
+                            val stT = pw.findViewById<TextView>(R.id.startTime)
+                            stT.text = musician.startTime
+                            stT.textSize = 10f
+                            stT.gravity = Gravity.CENTER
+                            val enT = pw.findViewById<TextView>(R.id.endTime)
+                            enT.textSize = 10f
+                            enT.text = musician.endTime
+
+
+                            val littleList = pw.findViewById<LinearLayout>(R.id.little_list)
+                            for (sing in musician.tracksList) {
+                                val newLayout = LinearLayout(pw.context)
+                                newLayout.layoutParams = LinearLayout.LayoutParams(
+                                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                                    LinearLayout.LayoutParams.MATCH_PARENT,
+                                    1f)
+
+                                val textView = TextView(pw.context)
+                                textView.layoutParams = LinearLayout.LayoutParams(
+                                    LinearLayout.LayoutParams.MATCH_PARENT,
+                                    LinearLayout.LayoutParams.MATCH_PARENT,
+                                    0.25f)
+                                textView.text = sing
+                                textView.setTextColor(argb(0xff, 0x00, 0x00, 0x00))
+                                textView.setPadding(58, 4, 4, 4)
+                                val typeface = ResourcesCompat.getFont(pw.context, R.font.roboto_regular)
+                                textView.setTypeface(typeface)
+
+                                val playingNowView = TextView(pw.context)
+                                playingNowView.layoutParams = LinearLayout.LayoutParams(
+                                    LinearLayout.LayoutParams.MATCH_PARENT,
+                                    LinearLayout.LayoutParams.MATCH_PARENT,
+                                    0.75f)
+                                playingNowView.setTextColor(argb(0xff, 0x00, 0x00, 0x00))
+                                playingNowView.setPadding(4, 4, 26, 4)
+                                playingNowView.setTypeface(typeface)
+
+                                newLayout.addView(textView)
+                                newLayout.addView(playingNowView)
+                                littleList.addView(newLayout)
+                            }
+                            val return_but = pw.findViewById<Button>(R.id.return_button)
+                            return_but.setOnClickListener {
+                                pw.dismiss()
+                            }
+
+
+
+//            <LinearLayout
+//            android:layout_width="match_parent"
+//            android:layout_height="wrap_content"
+//            android:weightSum="1">
+//            <TextView
+//            android:layout_width="match_parent"
+//            android:layout_height="match_parent"
+//            android:text="Face - West falling down"
+//            android:layout_weight="0.25"
+//            android:textColor="#FF000000"
+//            android:paddingLeft="48dp" android:fontFamily="@font/roboto_regular_sh"
+//
+//            />
+//            <TextView
+//            android:layout_width="match_parent"
+//            android:layout_height="match_parent"
+//            android:textColor="#FF000000"
+//            android:text="playing now"
+//
+//            android:layout_weight="0.75"
+//            android:gravity="right"
+//            android:paddingRight="26dp" android:fontFamily="@font/roboto_regular_sh"
+//
+//            />
+
+
+
+
+
+                            /////////////////////////////
+                        }
+
+                        override fun onTapEvent(pointF : PointF) : Boolean {
+                            /* show toast message for onPanEnd gesture callback */
+                            return false
+                        }
+
+                        override fun  onDoubleTapEvent(pointF : PointF) : Boolean {
+                            return false
+                        }
+
+                        override fun  onPinchLocked() {
+
+                        }
+
+                        override fun onPinchZoomEvent(v: Float , pointF : PointF) : Boolean {
+                            return false
+                        }
+
+                        override fun  onRotateLocked() {
+                        }
+
+                        override fun  onRotateEvent(v : Float) : Boolean {
+                            /* show toast message for onRotateEvent gesture callback */
+                            Log.i("Fuck", "onRotateEvent")
+                            return false
+                        }
+
+                        override fun onTiltEvent(v : Float) : Boolean {
+                            return false
+                        }
+
+                        override fun  onLongPressEvent(v : PointF) : Boolean {
+                            Log.i("Fuck", "onLongPressEvent")
+                            return false
+                        }
+
+                        override fun onLongPressRelease() : Unit {
+                        }
+
+                        override fun  onTwoFingerTapEvent( pointF : PointF) : Boolean {
+                            Log.i("Fuck", "onTwoFingerTapEvent")
+                            return false
+                        }
+                    }, 0, false)
+
+
+
+
                     map = mapFragment.map
                     map.setCenter(GeoCoordinate(59.9343, 30.3351), Map.Animation.NONE)
                     map.zoomLevel = (map.maxZoomLevel + map.minZoomLevel) / 2
@@ -103,7 +316,8 @@ class MainActivity : AppCompatActivity() {
                             musicians.forEach {
                                 val image = Image()
                                 image.bitmap = musicianIcon
-                                musiciansMarkers.add(MapMarker(GeoCoordinate(it.xCoord, it.yCoord), image))
+                                val marker = MapMarker(GeoCoordinate(it.xCoord, it.yCoord), image)
+                                musiciansMarkers.add(marker)
                                 Log.i("ForEach", it.name)
                             }
                             map.addMapObjects(musiciansMarkers.toList())
@@ -111,19 +325,22 @@ class MainActivity : AppCompatActivity() {
                             if (copyMusicians.size != 0)
                                 this@MainActivity.runOnUiThread {
                                     findViewById<RecyclerView>(R.id.list).apply {
-                                            adapter = MusicianAdapter(copyMusicians)
+                                            adapter = MusicianAdapter(copyMusicians, this@MainActivity, windowManager)
                                             layoutManager = LinearLayoutManager(this@MainActivity)
                                     }
                                 }
                             Thread.sleep(10000)
                         }
                     }.start()
+
                 }
                 else {
                     Log.e("map.init", it.name)
                 }
+
             }
         }
+
     }
 
     private class GrpcTask constructor(_musicians: ArrayList<Musician>) : AsyncTask<Void, Void, String>() {
@@ -131,8 +348,8 @@ class MainActivity : AppCompatActivity() {
         private var channel: ManagedChannel? = null
 
         override fun doInBackground(vararg poof: Void) : String {
-//            val host = "10.100.110.201"
-            val host = "192.168.43.230"
+            val host = "10.100.110.201"
+//            val host = "192.168.43.230"
             val port = 50051
             return try {
                 channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext().build()
--- client/src/main/java/ru/ifmo/se/client/MusicianAdapter.kt
+++ client/src/main/java/ru/ifmo/se/client/MusicianAdapter.kt
@@ -1,12 +1,22 @@
 package ru.ifmo.se.client
+import android.app.Dialog
+import android.content.Context
+import android.graphics.Color.argb
+import android.graphics.Point
+import android.support.v4.content.res.ResourcesCompat
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
+import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
 import ru.ifmo.se.protofiles.Musician
 
-class MusicianAdapter(private val list: Array<Musician>): RecyclerView.Adapter<MusicianAdapter.MusicianHolder>() {
+
+
+class MusicianAdapter(private val list: Array<Musician>, val context: Context, val windowManaer: WindowManager): RecyclerView.Adapter<MusicianAdapter.MusicianHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MusicianHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.list_row, parent, false)
@@ -23,9 +33,98 @@ class MusicianAdapter(private val list: Array<Musician>): RecyclerView.Adapter<M
         holder.types.text = musician.generesList.joinToString()
         holder.startTime.text = musician.startTime
         holder.endTime.text = musician.endTime
+
+
+        holder.view.setOnClickListener({
+            val pw = Dialog(context, android.R.style.Theme_Translucent_NoTitleBar)
+            pw.setContentView(R.layout.autor)
+            pw.getWindow().setLayout(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.FILL_PARENT);
+            pw.show()
+
+
+            val singerName = pw.findViewById<TextView>(R.id.singer_name)
+            singerName.text = musician.name
+
+            val singer_styles = pw.findViewById<TextView>(R.id.singer_styles)
+            singer_styles.text = musician.generesList.joinToString()
+
+            val stT = pw.findViewById<TextView>(R.id.startTime)
+            stT.text = musician.startTime
+            val enT = pw.findViewById<TextView>(R.id.endTime)
+            enT.text = musician.endTime
+
+
+            val littleList = pw.findViewById<LinearLayout>(R.id.little_list)
+            for (sing in musician.tracksList) {
+                val newLayout = LinearLayout(pw.context)
+                newLayout.layoutParams = LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    1f)
+
+                val textView = TextView(pw.context)
+                textView.layoutParams = LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    0.25f)
+                textView.text = sing
+                textView.setTextColor(argb(0xff, 0x00, 0x00, 0x00))
+                textView.setPadding(58, 4, 4, 4)
+                val typeface = ResourcesCompat.getFont(pw.context, R.font.roboto_regular)
+                textView.setTypeface(typeface)
+
+                val playingNowView = TextView(pw.context)
+                playingNowView.layoutParams = LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    0.75f)
+//                playingNowView.text = sing
+                playingNowView.setTextColor(argb(0xff, 0x00, 0x00, 0x00))
+                playingNowView.setPadding(4, 4, 26, 4)
+                playingNowView.setTypeface(typeface)
+
+                newLayout.addView(textView)
+                newLayout.addView(playingNowView)
+                littleList.addView(newLayout)
+            }
+
+
+
+//            <LinearLayout
+//            android:layout_width="match_parent"
+//            android:layout_height="wrap_content"
+//            android:weightSum="1">
+//            <TextView
+//            android:layout_width="match_parent"
+//            android:layout_height="match_parent"
+//            android:text="Face - West falling down"
+//            android:layout_weight="0.25"
+//            android:textColor="#FF000000"
+//            android:paddingLeft="48dp" android:fontFamily="@font/roboto_regular_sh"
+//
+//            />
+//            <TextView
+//            android:layout_width="match_parent"
+//            android:layout_height="match_parent"
+//            android:textColor="#FF000000"
+//            android:text="playing now"
+//
+//            android:layout_weight="0.75"
+//            android:gravity="right"
+//            android:paddingRight="26dp" android:fontFamily="@font/roboto_regular_sh"
+//
+//            />
+
+
+
+            val return_but = pw.findViewById<Button>(R.id.return_button)
+            return_but.setOnClickListener {
+                pw.dismiss()
+            }
+        })
     }
 
-    class MusicianHolder(view: View): RecyclerView.ViewHolder(view){
+    class MusicianHolder(val view: View): RecyclerView.ViewHolder(view){
         val name = view.findViewById<TextView>(R.id.musicianName)
         val types = view.findViewById<TextView>(R.id.musicianTypes)
         val startTime = view.findViewById<TextView>(R.id.startTime)
