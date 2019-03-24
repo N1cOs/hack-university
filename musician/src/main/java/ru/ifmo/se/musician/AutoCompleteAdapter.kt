56ba1899311298d77638f89ef26771a1dbfeedd3
Nick <nickkarmatskikh@gmail.com>
2019-03-24 05:57:17 +0300

    changed build file

---
+++ musician/src/main/java/ru/ifmo/se/musician/AutoCompleteAdapter.kt
@@ -0,0 +1,4 @@
+package ru.ifmo.se.musician
+
+class AutoCompleteAdapter {
+}
---
+++ musician/src/main/java/ru/ifmo/se/musician/MainMusician.kt
@@ -0,0 +1,12 @@
+package ru.ifmo.se.musician
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainMusician : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_musician)
+    }
+}
